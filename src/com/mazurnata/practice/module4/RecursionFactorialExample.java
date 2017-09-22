218d14e5c17fb068c50eef618d92d67d0aae80a1
Natalia <mazurnata84@gmail.com>
2017-09-22 16:40:47 +0300

    practice for module4

---
+++ src/com/mazurnata/practice/module4/RecursionFactorialExample.java
@@ -0,0 +1,24 @@
+package com.mazurnata.practice.module4;
+
+public class RecursionFactorialExample {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(factorial1(5));
+
+    }
+
+    static int factorial(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * factorial(x - 1);
+        }
+    }
+    static int factorial1(int x) {
+        int result = 1;
+        for (int i = 1; i <= x ; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
