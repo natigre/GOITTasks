2660dcaa3ef5858ea371f3b37118c5467978ba5b
Natalia <mazurnata84@gmail.com>
2017-10-11 09:43:03 +0300

    пример работы с кнопкой

--- src/com/mazurnata/practice/module06/JFXMain.java
+++ src/com/mazurnata/practice/module06/JFXMain.java
@@ -1,14 +1,15 @@
 package com.mazurnata.practice.module06;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-
-public class JFXMain extends Application {
+public class JFXMain extends Application implements EventHandler<ActionEvent> {
     Button button;
 
     public static void main(String[] args) {
@@ -19,10 +20,19 @@ public class JFXMain extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("First application");
         button = new Button("This is button");
+        button.setOnAction(this);   //задаем действия для кнопки
         StackPane layuot = new StackPane();
         layuot.getChildren().add(button);
         Scene scene = new Scene(layuot, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) { //прописываем условия для кнопки
+        // можем прописывать несколько кнопок
+        if (event.getSource() == button) {
+            System.out.println("Ayaa");//так не делают!!
+        }
     }
 }
