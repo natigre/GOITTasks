a0a7ea4ee1a5274822014d126e4cb4078bbc5d4c
Natalia <mazurnata84@gmail.com>
2017-11-19 18:03:24 +0200

    collection practice

---
+++ src/main/java/com/mazurnata/practice/module09/morePractice/ArrayListToArray.java
@@ -0,0 +1,25 @@
+package com.mazurnata.practice.module09.morePractice;
+
+import java.util.ArrayList;
+
+public class ArrayListToArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println("Contents of a list array: " + list);
+
+        //get common array
+        Integer ia[] = new Integer[list.size()];
+        ia = list.toArray(ia);
+
+        int sum = 0;
+        //get sum elements of array
+        for (int i : ia) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
