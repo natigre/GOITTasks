2014d75c40520e4507ae0357b616eb605d377442
Natalia <mazurnata84@gmail.com>
2017-11-19 19:19:37 +0200

    collection practice

--- src/main/java/com/mazurnata/practice/module09/morePractice/HashSetDemo.java
+++ src/main/java/com/mazurnata/practice/module09/morePractice/HashSetDemo.java
@@ -12,6 +12,6 @@ public class HashSetDemo {
         hashSet.add("Epsilon");
         hashSet.add("Omega");
         System.out.println(hashSet);
-        
+
     }
 }
---
+++ src/main/java/com/mazurnata/practice/module09/morePractice/LinkedHashSetDemo.java
@@ -0,0 +1,16 @@
+package com.mazurnata.practice.module09.morePractice;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        set.add("Beta");
+        set.add("Alfa");
+        set.add("Teta");
+        set.add("Gamma");
+        set.add("Epsilon");
+        set.add("Omega");
+        System.out.println(set);
+    }
+}
